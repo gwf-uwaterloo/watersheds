ba8943069fe2c2ae1790cf12eeb9fec355a3fbb6
d1shs0ap <42086645+d1shs0ap@users.noreply.github.com>
2022-03-28 14:21:02 -0400

    changed to search.app

--- watersheds/search/app/server/app.py
+++ watersheds/search/app/server/app.py
@@ -1,8 +1,9 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
-from ...watersheds._base import Basin
-from ...watersheds.search import get_geometries
+from _base import Basin
+from search.search import get_geometries
+
 
 app = Flask(__name__)
 CORS(app)
